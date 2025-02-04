aabb8fe36ed97e5fa0cea539c5b758c4d40456fc
Sonam157 <sonamourya7683@gmail.com>
2025-02-04 23:05:28 +0530

    created about us

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import RestaurantCard from "./component/RestaurantCard";
 import  Header from "./component/header.js";
 import Body from "./component/Body.js";
+import { createBrowserRouter } from "react-router-dom";
+import About from "./component/About.js"
+
+
 
 
 const AppLayout = () => {
@@ -13,6 +17,15 @@ const AppLayout = () => {
         </div>
     );
 };
-
+const appRouter = createBrowserRouter([
+    {
+        path:"/",
+        element:<AppLayout/>,
+    },
+    {
+        path:"/about",
+        element:<About/>
+    }
+])
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<AppLayout />);
---
+++ src/component/About.js
@@ -0,0 +1,8 @@
+const About =()=>{
+    (
+<h1>hello</h1>
+    )
+};
+
+
+export default About;
